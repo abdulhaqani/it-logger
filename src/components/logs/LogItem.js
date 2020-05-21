7dc9a29556a5121ed3ea6afa9260f80a457a2ba8
Abdul Haqani <ahaqa011@uottawa.ca>
2020-05-21 14:36:28 -0400

    Edit log modal

--- src/components/logs/LogItem.js
+++ src/components/logs/LogItem.js
@@ -7,7 +7,7 @@ const LogItem = ({ log }) => {
     <li className="collection-item">
       <div>
         <a
-          href="#eidt-log-modal"
+          href="#edit-log-modal"
           className={`modal-trigger ${
             log.attention ? 'red-text' : 'blue-text'
           }`}
