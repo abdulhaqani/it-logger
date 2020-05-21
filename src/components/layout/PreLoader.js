b8c60e00940761ec587d8fdc1838864327b8aa1e
Abdul Haqani <ahaqa011@uottawa.ca>
2020-05-21 02:38:15 -0400

    preloader configured, also cleaned up the logs

---
+++ src/components/layout/PreLoader.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const PreLoader = () => {
+  return (
+    <div className="progress blue lighten-4">
+      <div className="indeterminate blue"></div>
+    </div>
+  );
+};
+
+PreLoader.propTypes = {};
+
+export default PreLoader;
