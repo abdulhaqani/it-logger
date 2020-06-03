85c3eb6e03f2d293b0273a70cafacf8596723e27
Abdul Haqani <ahaqa011@uottawa.ca>
2020-06-03 02:18:04 -0400

    tech select options

--- src/components/logs/AddLogModal.js
+++ src/components/logs/AddLogModal.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import TechSelectOptions from '../techs/TechSelectOptions';
 import { connect } from 'react-redux';
 import M from 'materialize-css/dist/js/materialize.min.js';
 import PropTypes from 'prop-types';
@@ -58,9 +59,7 @@ const AddLogModal = ({ addLog }) => {
               <option value="" style={{ color: 'grey' }}>
                 Select Technition
               </option>
-              <option value="John Doe">John Doe</option>
-              <option value="Sam Smith">Sam Smith</option>
-              <option value="Sara Wilson">Sara Wilson</option>
+              <TechSelectOptions />
             </select>
           </div>
         </div>
